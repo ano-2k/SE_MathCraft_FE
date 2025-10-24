1975c7079baa51e9b9462ce3fc3051d36ceb051f
ano-2k <anoaadhik0914@gmail.com>
2025-10-24 19:45:17 +0530

    vite default setup

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://marcconrad.com/uob/banana/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
